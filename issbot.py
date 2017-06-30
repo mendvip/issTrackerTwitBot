b1c42bef468cfa8c33777ed952f371ec84db3a0a
daniel <danielnilssoncole@gmail.com>
2017-06-29 22:43:46 -0400

    stream listener set up

--- issbot.py
+++ issbot.py
@@ -7,8 +7,23 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    tweet_json_string = json.dumps(tweet._json, indent=4)
-    tweet_json_object = json.loads(tweet_json_string)
-    print tweet_json_string
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     # tweet_json_string = json.dumps(tweet._json, indent=4)
+#     # tweet_json_object = json.loads(tweet_json_string)
+#     # print tweet_json_string
+#     print tweet.text
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        if not status.retweeted:
+            username = status.user.screen_name
+            status_id = status.id
+            reply = 'thank you. we are still in testing.'
+            print 'test'
+            #api.update_status
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(track=['@issTrackerPy', '@isstrackerpy'], async=True)
